cd532ad7c385b817cb78c61bb88a4a3019e20ae4
DESTROYED-ATOM <164036812+DESTROYED-ATOM@users.noreply.github.com>
2024-03-21 08:34:49 +0530

    Update ATOM.py

--- ATOM.py
+++ ATOM.py
@@ -316,7 +316,7 @@ def _M1_(ids,pasx):
                 for ps in pasx:
                         session = requests.Session()
                         KING=random.choice(ugen) 
-                        free_fb = session.get('https://m.facebook.com').text
+                        BLACKX = session.get('https://m.facebook.com').text
                         info={'m_ts':re.search('name="m_ts" value="(.*?)"', str(BLACKX)).group(1), 
 			'li':re.search('name="li" value="(.*?)"', str(BLACKX)).group(1),
 			'try_number': '0', 
